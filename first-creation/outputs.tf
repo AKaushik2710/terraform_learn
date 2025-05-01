f024aa462b18c4ed4fb0fc6d8736559d9b3c4261
AKaushik2710 <159285365+AKaushik2710@users.noreply.github.com>
2025-05-01 22:14:35 +0530

    Added comments to outputs.tf

--- first-creation/outputs.tf
+++ first-creation/outputs.tf
@@ -1,7 +1,11 @@
+# This file is for storing output values and taking them into use
+
+# This value demonstrates domain name of instance 
 output "domain-name" {
   value = aws_instance.my-ec2.public_dns
 }
 
+# This value demonstrates IP address of instance 
 output "public-ip" {
   value = aws_instance.my-ec2.public_ip
 }
